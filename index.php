ef89920840043a4b9278e31ff9158c4aa29afe1c
Victor Jauregui <vicjaudom@hotmail.com>
2023-02-01 16:45:48 +0100

    doing 1st screen

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=Inria+Serif:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
+    <title>Gryfindor</title>
+</head>
+<body>
+    <div class="div-logo">
+        <img class="div-logo__image" src="assets/Gryffindor-Logo.png" alt="logo escuela de grifindor">
+    </div>
+    <div class="full-screen">
+        <div class="sidebar">
+            <div class="title-and-subjects">
+                <h2 id="subject">Subjects</h2>
+                <!-- <div class="types-of-subjects">
+                    <p class="types-of-subjects__subject" id="astronomy">Astronomy</p>
+                    <p class="types-of-subjects__subject" id="incatations">Incatations</p>
+                    <p class="types-of-subjects__subject" id="dark-arts">Dark arts</p>
+                    <p class="types-of-subjects__subject" id="defense-dark-art">Defense Dark arts</p>
+                    <p class="types-of-subjects__subject" id="flight">Flight</p>
+                    <p class="types-of-subjects__subject" id="herbology">Herbology</p>
+                </div>   -->
+            </div>
+            <div class="title-and-subjects">
+                <h2 class="h2-sidebar" id="calendary">Calendary</h2>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p class="name-admin">Dambledor</p>
+                <p class="type-admin">Teacher</p>
+            </div>
+        </div>
+        <div class="content-area">
+            <h2>Title</h2>
+            <div class="work-area">
+                <div class="subtitles">
+                    <h3 class="subtitles__h3">Name</h3>
+                    <h3 class="subtitles__h3">Last Name</h3>
+                    <h3 class="subtitles__h3">Email</h3>
+                    <h3 class="subtitles__h3">Califications</h3>
+                </div>
+                <div class="data-student">
+                    <div class="data-student__info-each-student" id="id-number">
+                        <p>1</p>
+                    </div>
+                    <div class="data-student__info-each-student" id="student-name">
+                        <p>Giorgi</p>
+                    </div>
+                    <div class="data-student__info-each-student" id="student-lastName">
+                        <p>Potter</p>
+                    </div>
+                    <div class="data-student__info-each-student" id="email">
+                        <p>email</p>
+                    </div>
+                    <div class="data-student__info-each-student" id="califications">
+                        <div class="trimester"></div>
+                        <div class="trimester"></div>
+                        <div class="trimester"></div>
+                    </div>
+                    <div class="info-each-student" id="">
+                        <img src="" alt="">
+                    </div>
+                </div>
+                <button class="btn-create-wizard">Create a new Wizard</button>
+                <button class="btn-save">Save</button>
+            </div>
+
+
+        </div>
+    </div>
+    
+</body>
+</html>
