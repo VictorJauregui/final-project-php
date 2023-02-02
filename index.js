9698c83213ef45d2b7133a7542887c603488a375
Victor Jauregui <vicjaudom@hotmail.com>
2023-02-02 10:17:17 +0100

    screen one finish

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ typesSubjects.addEventListener("click", subjectsSidebar);
 
 
 function subjectsSidebar(){
+
     if(subjects.classList.contains("types-of-subjects")){
-        subjects.classList.replace("types-of-subjects", "types-of-subjects-show");
+        subjects.classList.replace("types-of-subjects", "types-of-subjects-show")
+        typesSubjects.style.backgroundColor = "transparent";
+    } else if(subjects.classList.contains("types-of-subjects-show")){
+        subjects.classList.replace("types-of-subjects-show", "types-of-subjects")
+        typesSubjects.style.backgroundColor.hover = "red";
     }
-    console.log("hi");
 }
